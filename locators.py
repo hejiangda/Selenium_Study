88fd1892bf0eee93944640208c226c0b8c465125
hejiangda <hejiangda@163.com>
2020-02-15 13:46:34 +0800

    by LINK_TEXT

--- locators.py
+++ locators.py
@@ -2,3 +2,8 @@ from selenium.webdriver.common.by import By
 
 class WikipediaHomepages(object):
     Random_Link=(By.CSS_SELECTOR,'#n-randompage')
+
+class WikipediaArticle(object):
+    First_Heading=(By.CSS_SELECTOR,'.firstHeading')
+    Page_Info=(By.LINK_TEXT,'Page information')
+    Search_Box=(By.NAME,'search')
--- wiki.py
+++ wiki.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from locators import WikipediaHomepages
+from locators import WikipediaHomepages,WikipediaArticle
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver=webdriver.Chrome('./chromedriver')
@@ -10,5 +11,16 @@ random_link.click()
 
 time.sleep(4)
 
-print(driver.title)
+first_heading=driver.find_element(*WikipediaArticle.First_Heading)
+print(first_heading.text)
+page_info=driver.find_element(*WikipediaArticle.Page_Info)
+page_info.click();
+
+time.sleep(5)
+
+search_box=driver.find_element(*WikipediaArticle.Search_Box)
+search_box.send_keys('Selenium (software)' + Keys.RETURN)
+
+time.sleep(5)
+
 driver.quit()
